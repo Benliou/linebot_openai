33af462106cab21d903c58f5107d9bee4d0b23f3
Benliou <eggben0207@gmail.com>
2025-07-14 00:42:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,85 +1,111 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, request, abort
+from linebot.v3.webhook import WebhookHandler, Event
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging.models import TextMessage
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import (
+    MessageEvent, 
+    TextMessage, 
+    TextSendMessage,
+    ImageSendMessage)
+from linebot.exceptions import InvalidSignatureError
+import logging
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
+# 引入
+from places import get_nearby_restaurants
+from stock import txt_to_img_url
 
-#======python的函數庫==========
-import tempfile, os
-import datetime
-import openai
-import time
-import traceback
-#======python的函數庫==========
+import google.generativeai as genai
 
+# 加載 .env 文件中的變數
+load_dotenv()
+
+# 從環境變數中讀取 LINE 的 Channel Access Token 和 Channel Secret
+line_token = os.getenv('LINE_TOKEN')
+line_secret = os.getenv('LINE_SECRET')
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+model = genai.GenerativeModel("gemini-1.5-pro")
+
+# 檢查是否設置了環境變數
+if not line_token or not line_secret:
+    print(f"LINE_TOKEN: {line_token}")  # 調試輸出
+    print(f"LINE_SECRET: {line_secret}")  # 調試輸出
+    raise ValueError("LINE_TOKEN 或 LINE_SECRET 未設置")
+
+# 初始化 LineBotApi 和 WebhookHandler
+line_bot_api = LineBotApi(line_token)
+handler = WebhookHandler(line_secret)
+
+# 創建 Flask 應用
 app = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-# Channel Access Token
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-# Channel Secret
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
-# OPENAI API Key初始化設定
-openai.api_key = os.getenv('OPENAI_API_KEY')
 
+app.logger.setLevel(logging.DEBUG)
 
-def GPT_response(text):
-    # 接收回應
-    response = openai.Completion.create(model="gpt-3.5-turbo-instruct", prompt=text, temperature=0.5, max_tokens=500)
-    print(response)
-    # 重組回應
-    answer = response['choices'][0]['text'].replace('。','')
-    return answer
-
-
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
+# 設置一個路由來處理 LINE Webhook 的回調請求
+@app.route("/", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+    # 取得 X-Line-Signature 標頭
     signature = request.headers['X-Line-Signature']
-    # get request body as text
+
+    # 取得請求的原始內容
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
+    app.logger.info(f"Request body: {body}")
+
+    # 驗證簽名並處理請求
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+
     return 'OK'
 
-
-# 處理訊息
+# 設置一個事件處理器來處理 TextMessage 事件
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    msg = event.message.text
-    try:
-        GPT_answer = GPT_response(msg)
-        print(GPT_answer)
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
-    except:
-        print(traceback.format_exc())
-        line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
-        
+def handle_message(event: Event):
+    if event.message.type == "text":
+        user_message = event.message.text  # 使用者的訊息
+        app.logger.info(f"收到的訊息: {user_message}")
 
-@handler.add(PostbackEvent)
-def handle_message(event):
-    print(event.postback.data)
+        if user_message == "附近的餐廳":
+            reply_text = get_nearby_restaurants()
+        elif user_message == "課表":
+            reply_text = "這是你的課表～"
+        elif user_message == "台積電股票":
+            try:
+                image_url = txt_to_img_url()
+                if not image_url:
+                    error_message = f"抱歉，沒有取得股票趨勢圖，{image_url}。"
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextMessage(text=error_message)
+                    )
+                    return
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    ImageSendMessage(
+                        original_content_url=image_url,
+                        preview_image_url=image_url
+                    )
+                )
+            except Exception as e:
+                error_message = f"抱歉，無法生成股票趨勢圖，錯誤原因：{e}"
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextMessage(text=error_message)
+                )
+            return
+        else:
+            response = model.generate_content(user_message) # 傳送使用者的問題給 Gemini
+            reply_text = response.text if response else "抱歉，我無法回答這個問題。"
+         
 
-
-@handler.add(MemberJoinedEvent)
-def welcome(event):
-    uid = event.joined.members[0].user_id
-    gid = event.source.group_id
-    profile = line_bot_api.get_group_member_profile(gid, uid)
-    name = profile.display_name
-    message = TextSendMessage(text=f'{name}歡迎加入')
-    line_bot_api.reply_message(event.reply_token, message)
-        
-        
-import os
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_text)
+        )
+# 應用程序入口點
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
